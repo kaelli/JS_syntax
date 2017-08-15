9c1e72a267d0fbb4843cce5184a774be24421f20
kaelli <kaerli321@gmail.com>
2017-08-15 22:30:06 +0800

    方法，this,高阶函数，map,reduce

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-## JS_syntax DEMO 
+## JS_syntax
+学习地址[渺雪峰](https://www.liaoxuefeng.com/wiki/001434446689867b27157e896e74d51a89c25cc8b43bdb3000/001435119854495d29b9b3d7028477a96ed74db95032675000)
+* 方法，对象方法，this,
+* 高阶函数 array.map(fun(x)),array.reduce(fun(x,y))
--- demo/01.js
+++ demo/01.js
@@ -1,11 +1,48 @@
 //对象的属性 方法
+// var xiaoming = {
+//     name: '小明',
+//     birth: 1990,
+//     age: function () {
+//         var y = new Date().getFullYear();
+//         return y - this.birth;
+//     }
+// };
+// console.log(xiaoming.age); // function xiaoming.age()
+// console.log(xiaoming.age());  // 今年调用是25,明年调用就变成26了
+
+'use strict';
 var xiaoming = {
     name: '小明',
     birth: 1990,
     age: function () {
-        var y = new Date().getFullYear();
-        return y - this.birth;
+        var that = this; // 在方法内部一开始就捕获this
+        function getAgeFromBirth() {
+            var y = new Date().getFullYear();
+            return y - that.birth; // 用that而不是this
+        }
+        return getAgeFromBirth();
     }
 };
-console.log(xiaoming.age); // function xiaoming.age()
-console.log(xiaoming.age());  // 今年调用是25,明年调用就变成26了
+
+// xiaoming.age(); // 25
+
+/**
+ * map学习
+ * @type {[number,number,number,number,number]}
+ */
+var a = [1,2,3,4,5];
+var b = a.map(function (x) {
+    return x*x;
+})
+for (var key in b){
+    // console.log(b[key]);
+}
+
+/**
+ * reduce学习
+ */
+var c = [1,2,3,4,5];
+var d = c.reduce(function (p1, p2) {
+    return p1+p2;
+})
+console.log(d);
