d536c25e6a93892fba1fb8a9ef67a2ec3f21de2d
kaelli <kaerli321@gmail.com>
2017-08-16 22:35:19 +0800

    filter,sort,闭包学习

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ## JS_syntax
 学习地址[渺雪峰](https://www.liaoxuefeng.com/wiki/001434446689867b27157e896e74d51a89c25cc8b43bdb3000/001435119854495d29b9b3d7028477a96ed74db95032675000)
 * 方法，对象方法，this,
-* 高阶函数 array.map(fun(x)),array.reduce(fun(x,y))
-* test
+* 高阶函数 array.map(fun(x)),array.reduce(fun(x,y))，filter(),sort()
+* 闭包学习[阮大](http://www.ruanyifeng.com/blog/2009/08/learning_javascript_closures.html)
+  [渺雪峰闭包](https://www.liaoxuefeng.com/wiki/001434446689867b27157e896e74d51a89c25cc8b43bdb3000/0014345005399057070809cfaa347dfb7207900cfd116fb000)
--- demo/01.js
+++ demo/01.js
@@ -45,4 +45,91 @@ var c = [1,2,3,4,5];
 var d = c.reduce(function (p1, p2) {
     return p1+p2;
 })
-console.log(d);
+// console.log(d);
+
+/**
+ * filter
+ */
+var e = c.filter(function (x) {
+    return x%2!=0;
+})
+// console.log(e);
+
+c.filter(function (element, index,self) {
+    // console.log(element);
+    // console.log(index);
+    // console.log(self);
+    return true;
+});
+
+/**
+ * 质数过滤
+ * @param arr
+ * @returns {*|Array.<T>}
+ */
+function get_primes(arr) {
+    var fun = function (x) {
+        var i = 2;
+        if (x<2){
+            return false;
+        }
+        if (x==2){
+            return true;
+        }
+        while (i<x){
+            if (x%i==0){
+                return false
+            }
+            i++;
+        };
+        return true;
+    }
+    var result = arr.filter(function (x) {
+        return fun(x);
+    });
+    return result;
+}
+
+// 测试:
+var
+    x,
+    r,
+    arr = [];
+for (x = 1; x < 100; x++) {
+    arr.push(x);
+}
+r = get_primes(arr);
+if (r.toString() === [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97].toString()) {
+    // console.log('测试通过!');
+} else {
+    // console.log('测试失败: ' + r.toString());
+}
+
+/**
+ * sort 默认是根据ASCII
+ * sort 会把结果放到array中
+ */
+var sort_arr = ['Micsoft','Google','apple'];
+var sort_result = sort_arr.sort();
+// console.log(sort_result);
+var sort_result = [1,2,10,20].sort();
+// console.log(sort_result);
+console.log(sort_arr);
+
+/**
+ * == 和===的区别
+ *  定义："==="叫做严格运算符，"=="叫做相等运算符。
+ *  1.===当类型不同时，直接false
+ */
+
+
+/**
+ * 闭包：高阶函数的用法，1.参数可以是函数，2.返回值可以是函数，延时调用
+ *
+ * 理解：闭包就是能够读取其他函数内部变量的函数。
+ * 由于在Javascript语言中，只有函数内部的子函数才能读取局部变量，因此可以把闭包简单理解成"定义在一个函数内部的函数"。
+ * 所以，在本质上，闭包就是将函数内部和函数外部连接起来的一座桥梁。
+ *
+ * 用途：闭包可以用在许多地方。它的最大用处有两个，一个是前面提到的可以读取函数内部的变量，另一个就是让这些变量的值始终保持在内存中。
+ */
+
